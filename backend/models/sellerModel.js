334b14c0a4c11a9b54bff81529f3590ccaad7d17
Savindu Nawarathne <savindunawarathne2000@gmail.com>
2025-03-22 23:02:47 +0530

    seller modal

---
+++ backend/models/sellerModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  images: [String],
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  categories: {
+    type: String,
+    required: true,
+  },
+  seller_id: {
+    type: String,
+    required: true,
+  },
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
