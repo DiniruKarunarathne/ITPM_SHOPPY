91d5ba5984cf73a200bddad1258e990b12d60082
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-03-29 20:27:34 +0530

    Update server.js
    Update server.js - update 1

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const authRouter = require("./routes/auth.router.js");
+const router = require("./routes/sellerRoute.js");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv").config({ path: "./.env" });
+const port = process.env.PORT || 5000;
+
+// use middleware
+// use middleware
+app.use(
+  cors({
+    origin: "*", // Change this to the origin of your frontend application
+    // origin: 'http://localhost:3001',
+    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+// mongodb connection
+const con = require("./db/connection.js");
+
+// using routes
+//authentication route
+// app.use(authRouter);
+// app.use("/api/seller", require("./routes/sellerRoute.js"));
+// app.use("/api/admin", require("./routes/adminReview.Route.js"));
+
+
+app.use("/api/contactus", require("./routes/contactus.router.js"));
+
+// app.use(require("./routes/contactus.router.js"));
+
+
+// app.use(require("./routes/cart.router.js"));
+// app.use(require("./routes/review.route.js"));
+// app.use(require("./routes/payment.route.js"));
+// app.use(require("./routes/adminReview.Route.js"));
+
+
+// app.use("/api/admin", require("./routes/adminRoute.js"));
+
+// app.use("/api/item", require("./routes/itemsRoute.js"));
+
+
+con
+  .then((db) => {
+    if (!db) return process.exit(1);
+
+    // listen to the http server
+    const server = app.listen(port, () => {
+      console.log(`Server is running on port: http://localhost:${port}`);
+    });
+
+    app.on("error", (err) =>
+      console.log(`Failed To Connect with HTTP Server : ${err}`)
+    );
+    // error in mondb connection
+  })
+  .catch((error) => {
+    console.log(`Connection Failed...! ${error}`);
+  });
