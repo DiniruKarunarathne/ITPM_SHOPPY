f5334334f13a390f0c7f0b0481c53e0a58abce21
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-03-29 20:20:48 +0530

    Create contactus.model.js
    contactus.model.js - update 1

---
+++ backend/models/contactus.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+//Contact Us
+const Contactus_model = new Schema({
+    name:{type:String},
+    email:{type:String},
+    message:{type:String},
+    date:{type:String},
+});
+
+const Contactus = mongoose.model("contactus",Contactus_model);
+
+exports.default = Contactus;
+
+module.exports ={
+    Contactus,
+}
