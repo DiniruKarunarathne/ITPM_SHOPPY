84594b2eb459770aa9e8f5819836f8d2feab049a
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-04-30 13:23:30 +0530

    Update Home--

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Banner from '../../components/home/Banner'
-import ImageScanner from '../../components/home/ImageScanner'
+// import ImageScanner from '../../components/home/ImageScanner'
 import FooterSecondary from '../../components/FooterSecondary'
 import Card from '../../components/home/Card'
 
