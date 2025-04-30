d046ea58f7ee5e7134478ccd0bbba4058e5f6e73
Savindu Nawarathne <savindunawarathne2000@gmail.com>
2025-04-30 22:12:18 +0530

    fix merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,46 @@
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import { fetchAllCartItems } from "./services/redux/productSlice";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import CartPage from "./pages/Cart";
-// import CheckoutPage from "./pages/Checkout";
-// import ProductsPage from "./pages/Shoptest";
-import ContactUs from "./pages/ContactUs";
-import AdminContactUs from "./pages/Admincontact";
-// import AdminProducts from "./admin/admin-products";
-// import AdminOrders from "./admin/admin-order";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 import Home from "./pages/Home/Home";
 import PrimaryHeader from "./components/PrimaryHeader";
 import SecondaryHeader from "./components/SecondaryHeader";
-import Footer from "./components/home/Footer";
+import Seller from "./pages/Seller";
+import Customer from "./pages/Customer";
+import ContactUs from "./pages/ContactUs";
 
+import FooterSecondary from "./components/FooterSecondary";
+
+import Item from "./pages/seller/Item";
+import Selleritem from "./pages/seller/selleritem";
+import UpdateItem from "./pages/seller/UpdateItem";
+import Shop from "./pages/shop";
+import Menu from "./pages/cart/Menu";
+import AdminReview from "./pages/AdminReview";
+import Cart from "./pages/Cart";
+
+// import Review from "./components/Review";
+import SellerDashboard from "./pages/seller/SellerDashboard";
+// import StarReview from "./components/StarReview";
+import SellerReview from "./pages/seller/SellerReview";
+import { ToastContainer, toast } from "react-toastify";
+
+import Admincontact from "./pages/Admincontact";
+import Newshop from "./pages/Shop/Newshop";
+
+import Checkout from "./pages/Checkout";
 
 import AdminDashboard from "./pages/Admin/AdminDashboard";
-import AdminReview from "./pages/AdminReview";
+import Analysis from "./pages/analys";
+import AddReview from "./components/reviews/addReviews";
+
+
+
 
 function App() {
   const dispatch = useDispatch();
@@ -27,27 +51,63 @@ function App() {
   }, [dispatch]);
 
   return (
-
     <BrowserRouter>
-          <PrimaryHeader />
-      <SecondaryHeader /> 
+      <Toaster />
+      <PrimaryHeader />
+      <SecondaryHeader />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/seller" element={<Seller />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/cart" element={<Cart />} />
+
+        {/* <Route path="/review" element={<Review />} /> */}
+        <Route path="/reviewrating" element={<SellerReview />} />
+        <Route path="/adminReview" element={<AdminReview />} />
+
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/admin-contact" element={<Admincontact />} />
+        <Route path="/NewShop" element={<Newshop />} />
      
-        <Routes>
-        <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/admin/contactus" element={<AdminContactUs />} />
-          {/* <Route path="/admin/products" element={<AdminProducts />} /> */}
-          {/* <Route path="/admin/orders" element={<AdminOrders />} /> */}
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/Register" element={<Register />} /> */}
-          {/* <Route path="/cart" element={<CartPage />} /> */}
-          {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
-          {/* <Route path="/shop" element={<ProductsPage />} /> */}
+
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
+
+        <Route path="/item" element={<Item />} />
+        <Route path="/sellerDashboard" element={<SellerDashboard />} />
+        {/* <Route path="/star" element={<StarReview />} /> */}
+        <Route path="/selleritem" element={<Selleritem />} />
+        <Route path="/UpdateItem/:id/edit" element={<UpdateItem />} />
+
+        <Route path="/shop" element={<Shop />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="menu/:filterby" element={<Menu />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/anaysys" element={<Analysis />} />
+        <Route path="/addreviews" element={<AddReview />} />
+        <Route path="/addreviews/:filterby" element={<AddReview />} />
       </Routes>
-      <Footer />
+
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <div style={{ marginTop: "10%" }}>
+        <FooterSecondary />
+      </div>
     </BrowserRouter>
-
-
   );
 }
+
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import "./main.css";
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from "./App";
-import { AuthContextProvider } from "./context/authContext";
 import { Provider } from "react-redux";
+import store from "./services/redux/store";
+import { AuthContextProvider } from "./context/authContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <AuthContextProvider>
         <App />
-      </AuthContextProvider>
+        </AuthContextProvider>
     </React.StrictMode>
-    </Provider>
+  </Provider>
 );
