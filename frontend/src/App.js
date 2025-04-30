7faa1ae778e4a43eab14e9ed5a0088b00a5ba1d4
S.I.Chathuranga <134764717+SIChathuranga@users.noreply.github.com>
2025-04-30 13:38:36 +0530

    Update App.js--

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import CartPage from "./pages/Cart";
-import CheckoutPage from "./pages/Checkout";
-import ProductsPage from "./pages/Shoptest";
-import ContactUs from "./pages/contactus";
-import AdminContactUs from "./admin/admin-contactus";
-import AdminProducts from "./admin/admin-products";
-import AdminOrders from "./admin/admin-order";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import CartPage from "./pages/Cart";
+// import CheckoutPage from "./pages/Checkout";
+// import ProductsPage from "./pages/Shoptest";
+import ContactUs from "./pages/ContactUs";
+import AdminContactUs from "./pages/Admincontact";
+// import AdminProducts from "./admin/admin-products";
+// import AdminOrders from "./admin/admin-order";
 import Home from "./pages/Home/Home";
 import PrimaryHeader from "./components/PrimaryHeader";
 import SecondaryHeader from "./components/SecondaryHeader";
@@ -22,14 +22,14 @@ function App() {
         <Routes>
         <Route path="/contactus" element={<ContactUs />} />
           <Route path="/admin/contactus" element={<AdminContactUs />} />
-          <Route path="/admin/products" element={<AdminProducts />} />
-          <Route path="/admin/orders" element={<AdminOrders />} />
+          {/* <Route path="/admin/products" element={<AdminProducts />} /> */}
+          {/* <Route path="/admin/orders" element={<AdminOrders />} /> */}
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/shop" element={<ProductsPage />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/Register" element={<Register />} /> */}
+          {/* <Route path="/cart" element={<CartPage />} /> */}
+          {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
+          {/* <Route path="/shop" element={<ProductsPage />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
