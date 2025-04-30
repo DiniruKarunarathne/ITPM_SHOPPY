5880edec4c1c78c5b114db46e13ea03aff705631
Savindu Nawarathne <savindunawarathne2000@gmail.com>
2025-04-30 21:52:41 +0530

    fix merge bugs fix backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const authRouter = require("./routes/auth.router.js");
-// const router = require("./routes/sellerRoute.js");
-=======
-
 const router = require("./routes/sellerRoute.js");
->>>>>>> b730af62a49f8f4bd0fee3310cc3a538cbd39c75
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -18,12 +13,7 @@ const port = process.env.PORT || 5000;
 // use middleware
 app.use(
   cors({
-<<<<<<< HEAD
-    origin: "*", // Change this to the origin of your frontend application
-=======
-
-    origin: "http://localhost:3000",
->>>>>>> b730af62a49f8f4bd0fee3310cc3a538cbd39c75
+    origin: "http://localhost:3000", // Change this to the origin of your frontend application
     // origin: 'http://localhost:3001',
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
   })
@@ -37,11 +27,10 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 // mongodb connection
 const con = require("./db/connection.js");
 
-<<<<<<< HEAD
 // using routes
 //authentication route
 app.use(authRouter);
-// app.use("/api/seller", require("./routes/sellerRoute.js"));
+app.use("/api/seller", require("./routes/sellerRoute.js"));
 // app.use("/api/admin", require("./routes/adminReview.Route.js"));
 
 
@@ -50,24 +39,16 @@ app.use("/api/contactus", require("./routes/contactus.router.js"));
 app.use(require("./routes/contactus.router.js"));
 
 
-// app.use(require("./routes/cart.router.js"));
-// app.use(require("./routes/review.route.js"));
-// app.use(require("./routes/payment.route.js"));
-// app.use(require("./routes/adminReview.Route.js"));
-
-
-// app.use("/api/admin", require("./routes/adminRoute.js"));
-
-// app.use("/api/item", require("./routes/itemsRoute.js"));
-
-=======
 app.use(require("./routes/cart.router.js"));
+app.use(require("./routes/review.route.js"));
 app.use(require("./routes/payment.route.js"));
+app.use(require("./routes/adminReview.Route.js"));
+
+
+app.use("/api/admin", require("./routes/adminRoute.js"));
+
 app.use("/api/item", require("./routes/itemsRoute.js"));
-app.use("/api/seller", require("./routes/sellerRoute.js"));
 
-
->>>>>>> b730af62a49f8f4bd0fee3310cc3a538cbd39c75
 
 con
   .then((db) => {
